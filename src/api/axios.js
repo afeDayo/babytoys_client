6dd87120fece7c94e163d1a426007ba8eebda202
afeDayo <afedayo@gmail.com>
2024-12-28 17:43:19 +0100

    Register

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,3 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api"
+const axiosInstance = axios.create({
+  baseURL: "https://babytoys-server.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
--- src/pages/Secure/Register.jsx
+++ src/pages/Secure/Register.jsx
@@ -1,17 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Register.css";
+import axios from "../../api/axios";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const [success, setSuccess] = useState("");
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.email]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("/api/auth/signup", formData);
+
+      setSuccess("Registration successfull Please log in.");
+
+      setError("");
+    } catch (err) {
+      setError(err.response?.data?.message || "Registration failed");
+    }
+  };
+
   return (
     <div className="signup">
-      <form className="up-form" action="">
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {success && <p style={{ color: "green" }}>{success}</p>}
+
+      <form onSubmit={handleSubmit} className="up-form" action="">
         <h1 className="up-title">Register</h1>
         <div className="up-input">
-          <input type="text" placeholder="Email Address" />
-          <input type="text" placeholder="Password" />
-          <input type="text" placeholder="Repeat Password" />
-          <button>Create an account</button>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="repeatPassword"
+            placeholder="Repeat Password"
+            value={formData.repeatPassword}
+            onChange={handleChange}
+            required
+          />
+
+          <button type="submit">Create an account</button>
         </div>
 
         <div className="already">
