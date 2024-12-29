1ad6437d06999c4c588a1a3bde972ce7cc87752e
afeDayo <afedayo@gmail.com>
2024-12-29 12:44:20 +0100

    Shop

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -4,14 +4,12 @@ import { Link } from "react-router-dom";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-
   const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get("/api/products");
-
         setProducts(response.data);
       } catch (err) {
         setError(err.response?.data?.message || "Failed to fetch products");
@@ -33,14 +31,19 @@ const Shop = () => {
     <div>
       <h1>SHOP</h1>
       <div>
-        {products.map((product) => {
+        {products.map((product) => (
           <div key={product._id}>
-            <Link to={`/api/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
               <h2>{product.name}</h2>
             </Link>
             <p>{product.price}</p>
-          </div>;
-        })}
+            <img
+              src={product.image}
+              alt={product.name}
+              style={{ width: "150px", height: "150px" }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
